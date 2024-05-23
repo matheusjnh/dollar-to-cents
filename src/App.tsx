4a4f283aa5605ce39b082dc061d430c9a155baac
Matheus Santos <matheusjnh@gmail.com>
2024-05-22 21:38:40 -0300

    add conversion result

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import style from './App.module.css';
 
+import penny from './assets/penny.png';
+import nickel from './assets/nickel.png';
+import dime from './assets/dime.png';
+import quarter from './assets/quarter.png';
+
 function App() {
   return (
     <div className={style.layout}>
@@ -11,6 +16,48 @@ function App() {
         <div className={style.inputContainer}>
           <span className={style.dollarSign}>$</span>
           <input className={style.input} type="text" placeholder="Dollars..." />
+        </div>
+
+        <div className={style.result}>
+          <div className={style.resultCoin}>
+            <span>Penny</span>
+            <img
+              className={style.resultCoinImage}
+              src={penny}
+              alt="Penny coin"
+            />
+            <span className={style.resultCoinValue}>$0.01</span>
+            <span className={style.resultCoinCount}>2x</span>
+          </div>
+
+          <div className={style.resultCoin}>
+            <span>Nickel</span>
+            <img
+              className={style.resultCoinImage}
+              src={nickel}
+              alt="Nickel coin"
+            />
+            <span className={style.resultCoinValue}>$0.05</span>
+            <span className={style.resultCoinCount}>2x</span>
+          </div>
+
+          <div className={style.resultCoin}>
+            <span>Dime</span>
+            <img className={style.resultCoinImage} src={dime} alt="Dime coin" />
+            <span className={style.resultCoinValue}>$0.10</span>
+            <span className={style.resultCoinCount}>2x</span>
+          </div>
+
+          <div className={style.resultCoin}>
+            <span>Quarter</span>
+            <img
+              className={style.resultCoinImage}
+              src={quarter}
+              alt="Quarter coin"
+            />
+            <span className={style.resultCoinValue}>$0.25</span>
+            <span className={style.resultCoinCount}>2x</span>
+          </div>
         </div>
       </div>
     </div>
