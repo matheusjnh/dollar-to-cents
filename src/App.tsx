a6721fb1e0b9f1026196ec613939e36f44094979
Matheus Santos <matheusjnh@gmail.com>
2024-05-23 21:15:22 -0300

    create dollar to cents conversion

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,33 @@ import penny from './assets/penny.png';
 import nickel from './assets/nickel.png';
 import dime from './assets/dime.png';
 import quarter from './assets/quarter.png';
+import { useState } from 'react';
 
 function App() {
+  const [coins, setCoins] = useState({
+    penny: 0,
+    nickel: 0,
+    dime: 0,
+    quarter: 0,
+  });
+
+  const handleDollarInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const dollarValue = Number(event.target.value);
+
+    if (isNaN(dollarValue)) return;
+
+    let balance = Math.round(dollarValue * 100);
+    const quarter = Math.floor(balance / 25);
+    balance %= 25;
+    const dime = Math.floor(balance / 10);
+    balance %= 10;
+    const nickel = Math.floor(((balance % 25) % 10) / 5);
+    balance %= 5;
+    const penny = Math.floor(((balance % 25) % 10) % 5);
+
+    setCoins({ quarter, dime, nickel, penny });
+  };
+
   return (
     <div className={style.layout}>
       <div className={style.header}>
@@ -15,7 +40,12 @@ function App() {
       <div className={style.main}>
         <div className={style.inputContainer}>
           <span className={style.dollarSign}>$</span>
-          <input className={style.input} type="text" placeholder="Dollars..." />
+          <input
+            className={style.input}
+            type="text"
+            placeholder="Dollars..."
+            onInput={handleDollarInput}
+          />
         </div>
 
         <div className={style.result}>
@@ -27,7 +57,7 @@ function App() {
               alt="Penny coin"
             />
             <span className={style.resultCoinValue}>$0.01</span>
-            <span className={style.resultCoinCount}>2x</span>
+            <span className={style.resultCoinCount}>{coins.penny}x</span>
           </div>
 
           <div className={style.resultCoin}>
@@ -38,14 +68,14 @@ function App() {
               alt="Nickel coin"
             />
             <span className={style.resultCoinValue}>$0.05</span>
-            <span className={style.resultCoinCount}>2x</span>
+            <span className={style.resultCoinCount}>{coins.nickel}x</span>
           </div>
 
           <div className={style.resultCoin}>
             <span>Dime</span>
             <img className={style.resultCoinImage} src={dime} alt="Dime coin" />
             <span className={style.resultCoinValue}>$0.10</span>
-            <span className={style.resultCoinCount}>2x</span>
+            <span className={style.resultCoinCount}>{coins.dime}x</span>
           </div>
 
           <div className={style.resultCoin}>
@@ -56,7 +86,7 @@ function App() {
               alt="Quarter coin"
             />
             <span className={style.resultCoinValue}>$0.25</span>
-            <span className={style.resultCoinCount}>2x</span>
+            <span className={style.resultCoinCount}>{coins.quarter}x</span>
           </div>
         </div>
       </div>
